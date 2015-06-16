8e8f57b01e62002ae22fdd7f6e0d5d0bf9c8f506
janusmols <janusbm@gmail.com>
2015-06-16 14:38:24 +0200

    s

--- BMI Body Mass Index WatchKit Extension/Result.swift
+++ BMI Body Mass Index WatchKit Extension/Result.swift
@@ -84,6 +84,31 @@ class Result: WKInterfaceController {
             var BMI = (Weight / (Height * Height)) * 703
             NSLog("\(BMI)")
             resultLabel.setText(NSString(format: "Your BMI: %.01f", BMI) as String)
+            
+            if (BMI <= 18.5){
+                
+                aboutTheResultLabel.setText("Underweight")
+            }
+            if ((BMI >= 18.5) && (BMI <= 24.99)){
+                
+                aboutTheResultLabel.setText("Normal Weight")
+            }
+            if ((BMI >= 25.0) && (BMI <= 29.99)){
+                
+                aboutTheResultLabel.setText("Overweight")
+            }
+            if ((BMI >= 30.0) && (BMI <= 34.99)){
+                
+                aboutTheResultLabel.setText("Obesity (Class 1)")
+            }
+            if ((BMI >= 35.0) && (BMI <= 39.99)){
+                
+                aboutTheResultLabel.setText("Obesity (Class 2)")
+            }
+            if (BMI >= 40.0){
+                
+                aboutTheResultLabel.setText("Morbid Obesity")
+            }
 
         }
         
